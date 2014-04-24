d13a92b36d10488fe3ec402c217de1e5fee3f91c
AJ <42003122+code7885@users.noreply.github.com>
2014-04-24 10:28:15 +0400

    Adding the file system for ( translate and localization ) our website

--- include/lang.php
+++ include/lang.php
@@ -1,17 +1,21 @@
 <?php
+// Build 2D array to hold the lang , keyword to use in our application and relate langauges . 
 
 $lang=array(
+    // First Langauges 
     'en'=>array(
         'hello'=>'Hello',
         'my_name'=>'My name is ',
         'username'=>'Username'
     )
+    // Second Languages
     ,
      'pn'=>array(
         'hello'=>'سلام',
         'my_name'=>'اسم من هست ',
          'username'=>'کلمه زمز'
     )
+    // Third Langages
     ,
     'fr'=>array(
         'hello'=>'Bonjour',
--- lang_test.php
+++ lang_test.php
@@ -1,8 +1,12 @@
 <?php
+// This is only for test propose to check out translate process is working fine or not . 
 
+//Adding keyword array information 
 include ('include/lang.php');
+
+//Check get param , if there is not param the default langauge will set to English 
 $default=(empty($_GET['lang']))?'en':$_GET['lang'];
 
+// Final step is to get the related translat from the selected language by exact keyword . 
 echo $lang[$default]['hello'].','.$lang[$default]['my_name'];
-
 ?>
